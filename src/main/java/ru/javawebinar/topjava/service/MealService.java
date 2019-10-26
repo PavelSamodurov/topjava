fa4bb8e1756f51bf12cb80063ce302431736d339
PavelSamodurov <46554497+PavelSamodurov@users.noreply.github.com>
2019-10-27 01:25:36 +0300

    2nd step done.

--- src/main/java/ru/javawebinar/topjava/service/MealService.java
+++ src/main/java/ru/javawebinar/topjava/service/MealService.java
@@ -32,8 +32,8 @@ public class MealService {
 
     public List<Meal> getBetweenDates(@Nullable LocalDate startDate, @Nullable LocalDate endDate, int userId) {
         return repository.getBetween(
-                DateTimeUtil.createDateTime(startDate, LocalDate.MIN, LocalTime.MIN),
-                DateTimeUtil.createDateTime(endDate, LocalDate.MAX, LocalTime.MAX),
+                DateTimeUtil.createDateTime(startDate, LocalDate.of(1, 1, 1), LocalTime.MIN),
+                DateTimeUtil.createDateTime(endDate, LocalDate.of(3000, 1, 1), LocalTime.MAX),
                 userId);
     }
 
